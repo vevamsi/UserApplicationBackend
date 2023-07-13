a06dd6a06acd95a56fd27deadb1198b0e84bff4c
vevamsi <117005690+vevamsi@users.noreply.github.com>
2023-07-13 16:11:17 +0530

    Add files via upload

--- src/test/java/com/example/demo/UserLoginApplicationTests.java
+++ src/test/java/com/example/demo/UserLoginApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ContextConfiguration;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.example.demo.Repo.EmployeeRepo;
+import com.example.demo.model.Employee;
+@ContextConfiguration(classes = UserLoginApplication.class)
 @SpringBootTest
 class UserLoginApplicationTests {
-
+	@Autowired
+    EmployeeRepo er;
 	@Test
-	void contextLoads() {
+	void saveEmployee() {
+		Employee e=new Employee();
+		e.setEmployeename("harry");
+		e.setEmail("harry@gmail.com");
+		e.setPassword("5555");
+		er.save(e);
+		assertNotNull(er.findByEmail("harry@gmail.com"));
 	}
 
 }
